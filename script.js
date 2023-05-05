d370b426bd4295cfcf044844036fb95a2f3d6fef
rebaderbs <rderbak@gmail.com>
2023-05-05 11:24:03 -0500

    computer random selection function working

---
+++ script.js
@@ -0,0 +1,9 @@
+//randomly returns 'Rock', 'Paper', or 'Scissors'
+function getComputerChoice()    {
+    let computerChoice = ["rock", "paper", "scissors"];
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)]
+}
+
+function playRound(playerSelection, computerSelection)    {
+
+}
